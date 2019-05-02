21e53ee99050b31dcccba36280e383c026406f67
TGranjon <thomas.granjon@orange.fr>
2019-05-02 09:58:34 +0200

    Changed some comments

--- deepvoice3_pytorch/frontend/text/__init__.py
+++ deepvoice3_pytorch/frontend/text/__init__.py
@@ -27,8 +27,9 @@ def text_to_sequence(text, cleaner_names):
     sequence = []
     
     # Check for curly braces and treat their contents as ARPAbet:
+    print("from frontend.txt:",text)
     while len(text):
-        print("from frontend.txt:",text)
+        #print("from frontend.txt:",text)
         m = _curly_re.match(text)
         #print(text)
         if not m:
